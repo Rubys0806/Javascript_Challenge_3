76ec86c7e6c1b671b2d42c313b9048aa1dc568a9
Rubys0806 <rubys0806@gmail.com>
2022-02-22 16:53:48 -0700

    created a string of acceptable characters for password

--- Develop/script.js
+++ Develop/script.js
@@ -4,12 +4,20 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
+var numericChar = "0123456789";
+var specialChar = "!#$%&'()*+-./:;<=>?@[\^_`{|}~";
+
 // Generate password function
 function generatePassword() {
    window.alert("Please choose the criteria you wish to include in your password.");
 while (true) { 
+  newPassword = '';
+  selectedChar = '';
+
   var passwordLength = window.prompt("How many characters would you like your password to be? (Password must be between 8-128 characters.)");
-  if (passwordLength == null) {
+  if (passwordLength === null) {
     break;
   }
   passwordLengthInteger= parseInt(passwordLength);
@@ -17,13 +25,35 @@ while (true) {
     window.alert("Please choose a valid password length between 8-128 characters.");
     continue;
   }
-var lowerCase = window.confirm("Would you like lowercase letters in your password?");
- 
-var upperCase = window.confirm("Would you like uppercase letters in your password?");
+var lowercase = window.confirm("Would you like lowercase letters in your password?");
+if (lowercase) {
+  selectedChar += lowercaseChar
+}
+
+var uppercase = window.confirm("Would you like uppercase letters in your password?");
+if (uppercase) {
+  selectedChar += uppercaseChar
+}
 
 var numeric = window.confirm("Would you like numbers in your password?");
+if (numeric) {
+  selectedChar += numericChar
+}
 
-var specialChar = window.confirm("Would you like special characters in your password?")
+var special = window.confirm("Would you like special characters in your password?")
+if (special) {
+  selectedChar += specialChar
+}
+
+if (lowercase === true || uppercase === true || numeric === true || special === true) {
+
+}
+
+else {
+  window.alert("You need to choose at least one criteria to meet for your password.")
+  continue; 
+}
+
 
 break;
 }
@@ -34,7 +64,6 @@ break;
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
